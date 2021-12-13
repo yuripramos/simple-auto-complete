b2b14d947df7b79407ac8c6efff9d9948dd95c60
Yuri Ramos <yuri.pramos@gmail.com>
2021-12-13 13:43:52 -0300

    Migrate App to hooks

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { fetchData } from "./api";
 import Autocomplete from "./components/Autocomplete";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { listOfCountries: [] };
-  }
+const App = () => {
+  const [listOfCountries, setListOfCountries] = useState([]);
 
-  async componentDidMount() {
-    this.setState({
-      listOfCountries: await fetchData(
-        "https://mocki.io/v1/cf3a3331-ea3f-4a82-88f8-db66ddb780b4"
-      ),
-    });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <content>
-          <Autocomplete options={this.state.listOfCountries} />
-        </content>
-      </div>
+  useEffect(async () => {
+    const result = await fetchData(
+      "https://mocki.io/v1/cf3a3331-ea3f-4a82-88f8-db66ddb780b4"
     );
-  }
-}
+
+    setListOfCountries(result);
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <content>
+        <Autocomplete options={listOfCountries} />
+      </content>
+    </div>
+  );
+};
 
 export default App;
